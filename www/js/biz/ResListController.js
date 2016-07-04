dd98811bb31ebce2b792717ab70ed9774648afe7
Tony <397013181@qq.com>
2016-07-04 17:35:24 +0800

    信息资源大分类数据绑定 九宫格使用

---
+++ www/js/biz/ResListController.js
@@ -0,0 +1,88 @@
+﻿angular.module('starter.ResCategoryController',[
+  'ngCordova',
+  'ionic',
+  'starter.CommentUtilsService',
+  'starter.ResService'
+  ])
+
+.controller('ResCategoryCtrl', ['$scope',
+  '$state',
+  '$ionicPlatform',
+  '$ionicHistory',
+  'CommentUtils',
+  'ResService',
+  function ($scope,$state,$ionicPlatform,$ionicHistory,CommentUtils,ResService) {
+    // android 返回按钮
+    $ionicPlatform.onHardwareBackButton(function(){
+      $ionicHistory.goBack();
+    });
+    $scope.mTitle = "";
+
+
+    $scope.ctrl = {
+      // 数据集
+      list: [],
+      listAll: [],
+      isFrist:true,
+
+      reset: function(){
+        $scope.ctrl.list = [];
+        $scope.ctrl.isFrist = true;
+
+      },
+      init:function(){
+        $scope.ctrl.getCateList();
+      },
+      getCateList:function(){
+        ResService.getCategories().then(function(response){
+          $scope.ctrl.isFrist = false;
+          if("InvaildToken" === response){
+            // 需要登录
+            CommentUtils.t.showToast("您还没有登录...");
+          }else{
+            var objResponse = JSON.parse(response);
+            var size = objResponse.length;
+            for (var n = 0; n < size; n++) {
+              var obj = objResponse[n];
+              var entity = {
+                ServerId:obj.ServerId,
+                Name : obj.Name,
+                ParentId : obj.ParentId,
+                ParentName : obj.ParentName,
+                OrderNum : obj.OrderNum,
+                IsDeleted : obj.IsDeleted,
+                UpdateTime : obj.UpdateTime};
+              $scope.ctrl.listAll.push(entity);
+            }
+            var l = [];
+            for(var n = 0; n < size; n++){
+              var obj = $scope.ctrl.listAll[n];
+              if(obj.ParentId == 0){
+                l.push(obj);
+              }
+            }
+            var lSize = l.length / 2;
+            for(var n = 0; n < lSize; n++){
+              var obj = {
+                left:l[n],
+                right:l[n+1]
+              };
+              $scope.ctrl.list.push(obj);
+            }
+            if(l.length % 2 > 0){
+              var obj = {
+                left:l[l.length - 1],
+                right:{Name:""}
+              };
+              $scope.ctrl.list.push(obj);
+            }
+          }
+        });
+      }
+
+    }
+
+
+
+    $scope.ctrl.init();
+}])
---
+++ www/js/biz/ResService.js
@@ -0,0 +1,21 @@
+﻿angular.module('starter.ResService',
+  [ 'angularSoap',
+  'starter.globalservice'])
+
+.factory("ResService", ['$soap',
+  'GlobalSetting',
+  function ($soap,GlobalSetting) {
+  return {
+    /**
+     * 获取资讯信息类别
+     */
+    getCategories: function () {
+      return $soap.post(GlobalSetting.getUrlBusiness(), "GetInfoCategories", {
+        updateTime:"2001-1-1",
+        userId: GlobalSetting.getUserId(),
+        token: GlobalSetting.getUserToken(),
+      });
+    }
+
+  }
+}]);
